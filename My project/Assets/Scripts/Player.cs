366539a5ba450c574f642deaf013879d2205b253
安達 廉仁 <240333@jc-21.jp>
2026-07-10 14:15:31 +0900

    ステージのスケール実寸大にした＆プレイヤー移動実装

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class Player
+
+
+public class PlayerMovement : MonoBehaviour
 {
     
+    public float speed = 5f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Debug.Log("Update動作中");
+        Debug.Log($"x={Input.GetAxis("Horizontal")}, z={Input.GetAxis("Vertical")}");
+
+        Vector3 movement = new Vector3(x, 0f, z);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
 }
